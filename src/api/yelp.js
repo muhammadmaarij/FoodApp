ba40ea0067fcf343237b836e08275fce8eecea26
Muhammad Maarij <84044169+muhammadmaarij@users.noreply.github.com>
2022-09-06 14:36:35 +0500

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,13 +4,7 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer mlotp3GIFZf-YmgzjOfCbILsGVKnmZdNSTfH7mK0Ii4W6wGyTUrsWOhnLFRZeIVCdCpYFuRndXvtgqRQBVNqLt0svTBWwy5mnPANLuUl8IZeiFT_06_sXeTSEGUUY3Yx",
+      "Bearer mlotp3GIFZf-YmgzjOfCbILsGVKnmZdNSTfH7mK0Ii4W6wGyTUrsWOhnLFRZeIVCdCpYFuRndXNqLt0svTBWwy5mnPANLuUl8IZeiFT_06_sXeTSEGx",
   },
 });
 
-// yelp.get("/search");
-// Client ID
-// e4x-mmNiFcLRkDy0HdyXgg
-
-// API Key
-// mlotp3GIFZf-YmgzjOfCbILsGVKnmZdNSTfH7mK0Ii4W6wGyTUrsWOhnLFRZeIVCdCpYFuRndXvtgqRQBVNqLt0svTBWwy5mnPANLuUl8IZeiFT_06_sXeTSEGUUY3Yx
